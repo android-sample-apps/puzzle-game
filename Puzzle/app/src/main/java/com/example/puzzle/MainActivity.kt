54e79b0f0fceb5871877f779f2d053c3c1b8f58f
kym1924 <kym1924@naver.com>
2020-12-10 14:05:26 +0900

    Add PuzzleAdapter

--- Puzzle/app/src/main/java/com/example/puzzle/MainActivity.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.puzzle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvPuzzle = findViewById<RecyclerView>(R.id.rv_puzzle)
+        val adapter = PuzzleAdapter()
+        rvPuzzle.adapter = adapter
+        rvPuzzle.horizontalDivider()
+        rvPuzzle.verticalDivider()
+        val list = mutableListOf(1,2,3,4,5,6,7,8,9)
+        adapter.setPuzzle(list)
+        adapter.shuffle(list)
+    }
+
+    private fun RecyclerView.horizontalDivider() {
+        val divider = DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL)
+        this.addItemDecoration(divider)
+    }
+
+    private fun RecyclerView.verticalDivider() {
+        val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        this.addItemDecoration(divider)
     }
 }
---
+++ Puzzle/app/src/main/java/com/example/puzzle/PuzzleAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.puzzle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PuzzleAdapter : RecyclerView.Adapter<PuzzleAdapter.PuzzleViewHolder>(){
+    private var puzzle = emptyList<Int>()
+
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int): PuzzleViewHolder
+        = PuzzleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_puzzle, parent, false))
+
+    override fun getItemCount() = puzzle.size
+
+    override fun onBindViewHolder(holder : PuzzleViewHolder, position : Int) {
+        holder.bind(puzzle[position])
+    }
+
+    fun setPuzzle(puzzle : List<Int>){
+        this.puzzle = puzzle
+        notifyDataSetChanged()
+    }
+
+    fun shuffle(puzzle : MutableList<Int>){
+        puzzle.shuffle()
+        this.puzzle = puzzle
+        notifyDataSetChanged()
+    }
+
+    inner class PuzzleViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(number : Int) {
+            itemView.findViewById<TextView>(R.id.tv_puzzle).text = number.toString()
+        }
+    }
+}
