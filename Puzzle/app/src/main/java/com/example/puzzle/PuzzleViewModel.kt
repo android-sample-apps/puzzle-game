635305664dccac5e950ee70de8728752d36dcd10
kym1924 <kym1924@naver.com>
2020-12-11 02:03:14 +0900

    Add if in PuzzleViewModel move

--- Puzzle/app/src/main/java/com/example/puzzle/PuzzleViewModel.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/PuzzleViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.puzzle
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -25,11 +26,12 @@ class PuzzleViewModel : ViewModel() {
 
     fun move(direction : Int) {
         val lastNumber = _puzzle.value!!.indexOf(max)
+        Log.d("tag", lastNumber.toString())
         when (direction) {
-            1 -> Collections.swap(temp, lastNumber, lastNumber+1) // left
-            2 -> Collections.swap(temp, lastNumber, lastNumber+3) // up
-            3 -> Collections.swap(temp, lastNumber, lastNumber-3) // down
-            4 -> Collections.swap(temp, lastNumber, lastNumber-1) // right
+            1 -> if(lastNumber%3!=2) Collections.swap(temp, lastNumber, lastNumber+1) // left
+            2 -> if(lastNumber<=6) Collections.swap(temp, lastNumber, lastNumber+3) // up
+            3 -> if(lastNumber>=3) Collections.swap(temp, lastNumber, lastNumber-3) // down
+            4 -> if(lastNumber%3!=0) Collections.swap(temp, lastNumber, lastNumber-1) // right
         }
         _puzzle.value = temp.toList()
     }
