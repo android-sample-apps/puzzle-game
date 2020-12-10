37f49747ae39beef070dc0ccf1c69fd72fc46bbf
kym1924 <kym1924@naver.com>
2020-12-10 23:12:52 +0900

    Add PuzzleViewModel

--- Puzzle/app/src/main/java/com/example/puzzle/MainActivity.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/MainActivity.kt
@@ -1,50 +1,27 @@
 package com.example.puzzle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
-import java.util.*
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.puzzle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel : PuzzleViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply{
+            puzzleViewModel = viewModel
+            lifecycleOwner = this@MainActivity
+        }
 
-        val rvPuzzle = findViewById<RecyclerView>(R.id.rv_puzzle)
+        viewModel.setPuzzle(9)
         val adapter = PuzzleAdapter()
-        rvPuzzle.adapter = adapter
+        binding.rvPuzzle.adapter = adapter
         val list = mutableListOf(1,2,3,4,5,6,7,8,9)
         adapter.setPuzzle(list)
         adapter.shuffle(list)
-
-        val btnLeft = findViewById<Button>(R.id.btn_left)
-        val btnUp = findViewById<Button>(R.id.btn_up)
-        val btnDown = findViewById<Button>(R.id.btn_down)
-        val btnRight = findViewById<Button>(R.id.btn_right)
-
-        btnLeft.setOnClickListener{
-            val lastNumber = list.indexOf(9)
-            Collections.swap(list, lastNumber, lastNumber+1)
-            adapter.setPuzzle(list)
-        }
-
-        btnUp.setOnClickListener{
-            val lastNumber = list.indexOf(9)
-            Collections.swap(list, lastNumber, lastNumber+3)
-            adapter.setPuzzle(list)
-        }
-
-        btnDown.setOnClickListener{
-            val lastNumber = list.indexOf(9)
-            Collections.swap(list, lastNumber, lastNumber-3)
-            adapter.setPuzzle(list)
-        }
-
-        btnRight.setOnClickListener{
-            val lastNumber = list.indexOf(9)
-            Collections.swap(list, lastNumber, lastNumber-1)
-            adapter.setPuzzle(list)
-        }
     }
 }
---
+++ Puzzle/app/src/main/java/com/example/puzzle/PuzzleViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.puzzle
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class PuzzleViewModel : ViewModel() {
+
+    private var max = 0
+
+    private val _puzzle = MutableLiveData<List<Int>>()
+    val puzzle : LiveData<List<Int>>
+        get() = _puzzle
+
+    private val temp = mutableListOf<Int>()
+
+    fun setPuzzle(max : Int) {
+        this.max = max
+        for(i in 1..max)
+            temp.add(i)
+        temp.shuffle()
+        _puzzle.value = temp
+    }
+
+    fun move(direction : Int) {
+        val lastNumber = _puzzle.value!!.indexOf(max)
+        when (direction) {
+            1 -> Collections.swap(temp, lastNumber, lastNumber+1) // left
+            2 -> Collections.swap(temp, lastNumber, lastNumber+3) // up
+            3 -> Collections.swap(temp, lastNumber, lastNumber-3) // down
+            4 -> Collections.swap(temp, lastNumber, lastNumber-1) // right
+        }
+        _puzzle.value = temp
+    }
+}
