c0fda1850c27eccdda23837452f3f0543f03f1c8
kym1924 <kym1924@naver.com>
2020-12-15 01:15:57 +0900

    Modify PuzzleAdapter for DataBinding

--- Puzzle/app/src/main/java/com/example/puzzle/PuzzleAdapter.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/PuzzleAdapter.kt
@@ -1,18 +1,18 @@
 package com.example.puzzle
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.puzzle.databinding.ItemPuzzleBinding
 
 class PuzzleAdapter : RecyclerView.Adapter<PuzzleAdapter.PuzzleViewHolder>(){
     private var puzzle = emptyList<Int>()
 
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int): PuzzleViewHolder
-        = PuzzleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_puzzle, parent, false))
+        = PuzzleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_puzzle, parent, false), puzzle.size)
 
     override fun getItemCount() = puzzle.size
 
@@ -27,11 +27,14 @@ class PuzzleAdapter : RecyclerView.Adapter<PuzzleAdapter.PuzzleViewHolder>(){
         diffResult.dispatchUpdatesTo(this)
     }
 
-    inner class PuzzleViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(number : Int) {
-            itemView.findViewById<TextView>(R.id.tv_puzzle).text = number.toString()
-            if(number == puzzle.size) itemView.setBackgroundColor(Color.WHITE)
-            else itemView.setBackgroundColor(Color.BLACK)
+    inner class PuzzleViewHolder(itemView : View, size : Int) : RecyclerView.ViewHolder(itemView) {
+
+        private val binding : ItemPuzzleBinding = DataBindingUtil.bind(itemView)!!
+        private val puzzleSize = size
+
+        fun bind(puzzle : Int) {
+            binding.setVariable(BR.puzzle, puzzle)
+            binding.setVariable(BR.size, puzzleSize)
         }
     }
 }
--- Puzzle/app/src/main/java/com/example/puzzle/PuzzleBinding.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/PuzzleBinding.kt
@@ -1,5 +1,7 @@
 package com.example.puzzle
 
+import android.graphics.Color
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,5 +10,16 @@ object PuzzleBinding {
     @JvmStatic
     fun setPuzzle(recyclerView : RecyclerView, puzzle : List<Int>?){
         if (recyclerView.adapter != null) with(recyclerView.adapter as PuzzleAdapter) { puzzle?.let {setPuzzle(puzzle)} }
+    }
+
+    @BindingAdapter("setNumber", "setColor")
+    @JvmStatic
+    fun setNumber(textView : TextView, number : Int?, size : Int?) {
+        if(size != number) {
+            textView.text = number.toString()
+            textView.setBackgroundColor(Color.BLACK)
+            textView.setTextColor(Color.WHITE)
+        }
+        else textView.setBackgroundColor(0)
     }
 }
