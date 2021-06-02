7a7a9b4d065add182ac2be2b474bac1b8ce76e06
kym1924 <kym1924@naver.com>
2021-06-02 17:58:34 +0900

    Add Back Button in PuzzleFragment

--- Puzzle/app/src/main/java/com/example/puzzle/puzzle/PuzzleFragment.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/puzzle/PuzzleFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.puzzle.R
 import com.example.puzzle.databinding.FragmentPuzzleBinding
 import com.example.puzzle.util.autoCleared
 
@@ -16,6 +18,13 @@ class PuzzleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPuzzleBinding.inflate(layoutInflater)
+        setBackToStart()
         return binding.root
+    }
+
+    private fun setBackToStart() {
+        binding.btnPuzzleBack.setOnClickListener {
+            requireView().findNavController().navigate(R.id.action_puzzleFragment_to_startFragment)
+        }
     }
 }
