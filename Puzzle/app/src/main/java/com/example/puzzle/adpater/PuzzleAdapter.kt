c0ce555cb75a61658e98049d97cb78bfbd17c7f0
kym1924 <kym1924@naver.com>
2021-06-02 18:05:56 +0900

    Refactor PuzzleAdapter

--- Puzzle/app/src/main/java/com/example/puzzle/adpater/PuzzleAdapter.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/adpater/PuzzleAdapter.kt
@@ -1,34 +1,32 @@
 package com.example.puzzle.adpater
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.puzzle.BR
-import com.example.puzzle.R
 import com.example.puzzle.databinding.ItemPuzzleBinding
 
 class PuzzleAdapter : ListAdapter<Int, PuzzleAdapter.PuzzleListViewHolder>(PuzzleDiffUtil()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PuzzleListViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_puzzle, parent, false),
-        currentList.size
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        PuzzleListViewHolder(
+            ItemPuzzleBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
     override fun onBindViewHolder(holder: PuzzleListViewHolder, position: Int) =
         holder.bind(getItem(position))
 
-    inner class PuzzleListViewHolder(itemView: View, private val size: Int) :
-        RecyclerView.ViewHolder(itemView) {
-
-        private val binding: ItemPuzzleBinding = DataBindingUtil.bind(itemView)!!
-
+    inner class PuzzleListViewHolder(private val binding: ItemPuzzleBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(puzzle: Int) {
             binding.setVariable(BR.puzzle, puzzle)
-            binding.setVariable(BR.size, size)
+            binding.setVariable(BR.size, currentList.size)
         }
     }
 
