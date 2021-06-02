2f32b230e5f2e1bd5a04820fbdee5459fd8848b1
kym1924 <kym1924@naver.com>
2021-06-02 19:07:50 +0900

    Add setSizeSelect in StartDialog

--- Puzzle/app/src/main/java/com/example/puzzle/start/StartDialog.kt
+++ Puzzle/app/src/main/java/com/example/puzzle/start/StartDialog.kt
@@ -5,11 +5,14 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
 import com.example.puzzle.databinding.DialogPuzzleBinding
 import com.example.puzzle.util.autoCleared
+import com.example.puzzle.viewmodel.PuzzleViewModel
 
 class StartDialog : DialogFragment() {
     private var binding by autoCleared<DialogPuzzleBinding>()
+    private val puzzleViewModel by activityViewModels<PuzzleViewModel>()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = DialogPuzzleBinding.inflate(requireActivity().layoutInflater)
@@ -20,12 +23,39 @@ class StartDialog : DialogFragment() {
         } ?: throw IllegalStateException()
     }
 
+    override fun onStart() {
+        super.onStart()
+        setSizeSelect()
+    }
+
     override fun onResume() {
         super.onResume()
         requireNotNull(dialog).apply {
-            requireNotNull(window).setLayout((resources.displayMetrics.widthPixels * 0.9).toInt(), ViewGroup.LayoutParams.WRAP_CONTENT)
+            requireNotNull(window).setLayout(
+                (resources.displayMetrics.widthPixels * 0.9).toInt(),
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
             setCancelable(false)
         }
+    }
+
+    private fun setSizeSelect() {
+        binding.apply {
+            tvDialogThree.setOnClickListener {
+                setPuzzle(3)
+            }
+            tvDialogFour.setOnClickListener {
+                setPuzzle(4)
+            }
+            tvDialogFive.setOnClickListener {
+                setPuzzle(5)
+            }
+        }
+    }
+
+    private fun setPuzzle(size: Int) {
+        puzzleViewModel.setPuzzle(size)
+        requireNotNull(dialog).dismiss()
     }
 
     companion object {
